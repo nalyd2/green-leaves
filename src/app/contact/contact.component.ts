e54baac33abe43e4744a913f7286af013f7925f0
efrain.252.robles@gmail.com <Dylanomar2@>
2018-08-01 18:13:38 -0500

    mensaje

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,21 +13,22 @@ export class ContactComponent implements OnInit {
     email:'',
     telefono:'',
     fecha:'',
-    ciudad:''
+    ciudad:'',
+    activo:false
  }
   constructor() { }
-
   ngOnInit() {
   }
   enviar(){
     let detalles = [];
     var rec = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var ret = /^([\(]?)+([\+]?)+([0-9]{2})+([\)]?)+([-]?)+([0-9]{3})+([-]?)+([0-9]{4})$/;
+    let activo = false;
 
     if (this.contacto.nombre.length == 0)
     detalles.push("Es necesario ingresar su nombre");
   if(!rec.test(this.contacto.email.toLowerCase()))
-    detalles.push("El formato de correo no es correcto.");
+    detalles.push("El formato de correo no es correcto, Ejemplo: someone@company.com");
   if (!ret.test(this.contacto.telefono))
     detalles.push("El número de teléfono no es correcto");
   if (this.contacto.fecha.length == 0)
@@ -38,6 +39,8 @@ export class ContactComponent implements OnInit {
   //this.popup.show();
     alert( detalles);
 
+    }else{
+      this.contacto.activo = true;
     }
 
   }
--- src/app/contacto.ts
+++ src/app/contacto.ts
@@ -4,4 +4,5 @@ export class Contacto{
     telefono:string;
     fecha:string;
     ciudad:string;
+    activo:boolean;
 }
